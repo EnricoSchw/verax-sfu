c14253028eca52a598714424624e31b7f2ebd619
EnricoSchw <enrico.schw@gmx.de>
2021-02-14 12:56:46 +0100

    Web socket server init

--- main.go
+++ main.go
@@ -5,21 +5,32 @@ import (
 	"net/http"
 )
 
-func main()  {
-
-	http.HandleFunc("/ws", func(writer http.ResponseWriter, request *http.Request) {
-		ws, err := NewWebSocket(writer, request)
-		if err != nil {
-			panic(err)
-		}
-		ws.On("message", func(event *Event) {
-			log.Printf("[MESSAGE] %v", event.Data)
-			ws.Out <- (&Event{
-				Name: "response",
-				Data: event.Data,
-			}).Raw()
-		})
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Get New connection ...%w", w)
+	ws, err := NewWebSocket(w, r)
+	if err != nil {
+		panic(err)
+	}
+	ws.On("message", func(event *Event) {
+		log.Printf("[MESSAGE] %v", event.Data)
+		ws.Out <- (&Event{
+			Name: "response",
+			Data: event.Data,
+		}).Raw()
 	})
 
-	http.ListenAndServe("8080", nil)
+}
+
+//type longLatStruct struct {
+//	Long float64 `json:"longitude"`
+//	Lat  float64 `json:"latitude"`
+//}
+//
+//var clients = make(map[*websocket.Conn]bool)
+//var broadcast = make(chan *longLatStruct)
+
+func main() {
+	log.Printf("Start Server...")
+	http.HandleFunc("/ws", wsEndpoint)
+	log.Fatal(http.ListenAndServe(":8999", nil))
 }
